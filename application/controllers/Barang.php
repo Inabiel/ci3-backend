1e1070ef75053ff7d55a19072717ea25c3e45a02
Inabiel <nabiel.p@students.amikom.ac.id>
2021-06-15 14:05:46 +0700

    i swear

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -1,9 +1,12 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+	//Nabiel Izzullah Pansuri -- 19.01.4419
+defined('BASEPATH') or exit('No direct script access allowed');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, OPTIONS");
-class Barang extends CI_Controller {	
-	public function list_barang(){
+class Barang extends CI_Controller
+{
+	public function list_barang()
+	{
 		$this->load->model('Barang_model');
 		$this->load->database();
 		$data_barang = $this->Barang_model->get_barang();
@@ -15,33 +18,36 @@ class Barang extends CI_Controller {
 				   </tr>
 		';
 
-		foreach ($data_barang->result() as $key=>$value){
+		foreach ($data_barang->result() as $key => $value) {
 			$konten .= '<tr>
-						<td>'.$value->nama_barang.'</td>
-						<td>'.$value->deskripsi.'</td>		
-						<td>'.$value->stok.'</td>
-						<td>Read | Hapus | <a href="#'.$value->id_barang.'" class="linkEditBarang">Edit</a></td>
+						<td>' . $value->nama_barang . '</td>
+						<td>' . $value->deskripsi . '</td>		
+						<td>' . $value->stok . '</td>
+						<td>Read | Hapus | <a href="#' . $value->id_barang . '" class="linkEditBarang">Edit</a></td>
 					</tr>
 			';
 		}
 
-		$data_json=array(
-			'konten'=>$konten
+		$data_json = array(
+			'konten' => $konten
 		);
 
 		echo json_encode($data_json);
 	}
-	//Nabiel Izzullah Pansuri -- 19.01.4419
-	public function create_action() {
+	public function create_action()
+	{
 		$this->load->model('Barang_model');
 		$this->db->trans_start();
 		$arr_input = array(
 			'nama_barang' => $this->input->post('nama_barang'),
-			'deskripsi' => $this->input->post('deskripsi')
+			'deskripsi' => $this->input->post('deskripsi'),
+			'nama_admin' => $this->input->post('nama_admin'),
+			'tanggal_barang_masuk' => $this->input->post('tanggal_barang_masuk'),
+			'harga_barang' => $this->input->post('harga_barang'),
 		);
 
 		$this->Barang_model->insert_data($arr_input);
-		if($this->db->trans_status() === FALSE){
+		if ($this->db->trans_status() === FALSE) {
 			$this->db->trans_rollback();
 			$data_output = array(
 				'sukses' => 'tidak',
@@ -57,17 +63,18 @@ class Barang extends CI_Controller {
 		echo json_encode($data_output);
 	}
 
-	public function update_action() {
+	public function update_action()
+	{
 		$this->db->trans_start();
 		$id_barang = $this->input->post('id_barang');
 		$arr_input = array(
 			'nama_barang' => $this->input->post('nama_barang'),
 			'deskripsi' => $this->input->post('deskripsi')
 		);
-		
+
 		$this->load->model('Barang_model');
-		$this->Barang_model->update_data($id_barang,$arr_input);
-		if($this->db->trans_status() === FALSE){
+		$this->Barang_model->update_data($id_barang, $arr_input);
+		if ($this->db->trans_status() === FALSE) {
 			$this->db->trans_rollback();
 			$data_output = array(
 				'sukses' => 'tidak',
@@ -83,23 +90,22 @@ class Barang extends CI_Controller {
 		echo json_encode($data_output);
 	}
 
-	public function detail() {
+	public function detail()
+	{
 		$id = $this->input->get('id_barang');
 		$this->load->model('Barang_model');
 		$data_detail = $this->Barang_model->get_by_id($id);
-		if($data_detail -> num_rows() > 0){
+		if ($data_detail->num_rows() > 0) {
 			$data_output = array(
 				'sukses' => 'ya',
 				'detail' => $data_detail->row_array()
 			);
-		}
-		else{
+		} else {
 			$data_output = array(
-				'sukses'=>'tidak'
+				'sukses' => 'tidak'
 			);
 		}
 
 		echo json_encode($data_output);
 	}
-
 }
