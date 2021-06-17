ca78d291ff7afd67053f044b972a469d5671a019
Inabiel <nabiel.p@students.amikom.ac.id>
2021-06-17 20:26:50 +0700

    did some job. yay

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -5,9 +5,13 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 class Barang extends CI_Controller
 {
+	public function __construct()
+	{
+			parent::__construct();
+			$this->load->model('Barang_model');
+	}
 	public function list_barang()
 	{
-		$this->load->model('Barang_model');
 		$this->load->database();
 		$data_barang = $this->Barang_model->get_barang();
 		$konten = '<tr>
@@ -23,7 +27,7 @@ class Barang extends CI_Controller
 						<td>' . $value->nama_barang . '</td>
 						<td>' . $value->deskripsi . '</td>		
 						<td>' . $value->stok . '</td>
-						<td>Read | Hapus | <a href="#' . $value->id_barang . '" class="linkEditBarang">Edit</a></td>
+						<td>Read | <a href="#' . $value->id_barang . '" class="linkHapusBarang">Hapus</a> | <a href="#' . $value->id_barang . '" class="linkEditBarang">Edit</a></td>
 					</tr>
 			';
 		}
@@ -36,7 +40,6 @@ class Barang extends CI_Controller
 	}
 	public function create_action()
 	{
-		$this->load->model('Barang_model');
 		$this->db->trans_start();
 		$arr_input = array(
 			'nama_barang' => $this->input->post('nama_barang'),
@@ -72,7 +75,6 @@ class Barang extends CI_Controller
 			'deskripsi' => $this->input->post('deskripsi')
 		);
 
-		$this->load->model('Barang_model');
 		$this->Barang_model->update_data($id_barang, $arr_input);
 		if ($this->db->trans_status() === FALSE) {
 			$this->db->trans_rollback();
@@ -93,7 +95,6 @@ class Barang extends CI_Controller
 	public function detail()
 	{
 		$id = $this->input->get('id_barang');
-		$this->load->model('Barang_model');
 		$data_detail = $this->Barang_model->get_by_id($id);
 		if ($data_detail->num_rows() > 0) {
 			$data_output = array(
@@ -107,5 +108,75 @@ class Barang extends CI_Controller
 		}
 
 		echo json_encode($data_output);
+	}
+
+	public function delete_data() {
+		$this->db->trans_start();
+		$id = $this->input->get('id_barang');
+		$this->Barang_model->hapus_data($id);
+		if ($this->db->trans_status() === FALSE) {
+			$this->db->trans_rollback();
+			$data_output = array(
+				'sukses' => 'tidak',
+				'pesan' => 'gagal delete data barang'
+			);
+		}
+		$this->db->trans_commit();
+		$data_output = array(
+			'sukses' => 'ya',
+			'pesan' => 'berhasil delete data barang'
+		);
+
+		echo json_encode($data_output);
+	}
+
+	public function soft_delete_data(){
+		$this->db->trans_start();
+		$id = $this->input->get('id_barang');
+		$this->Barang_model->soft_delete_data($id);
+		if ($this->db->trans_status() === FALSE) {
+			$this->db->trans_rollback();
+			$data_output = array(
+				'sukses' => 'tidak',
+				'pesan' => 'gagal delete data barang'
+			);
+		}
+		$this->db->trans_commit();
+		$data_output = array(
+			'sukses' => 'ya',
+			'pesan' => 'berhasil delete data barang'
+		);
+
+		echo json_encode($data_output);
+	}	
+
+	public function cari_barang() {
+		$nama = $this->input->post('cari_nama');
+		$desk = $this->input->post('cari_desk');
+		$stok = $this->input->post('cari_stok');
+		$data_barang = $this->Barang_model->get_barang($nama, $desk, $stok);
+		$konten = '<tr>
+				   <td>nama</td>
+				   <td>Deskripsi</td>
+				   <td>Stok</td>
+				   <td>aksi</td>
+				   </tr>
+		';
+
+		foreach ($data_barang->result() as $key => $value) {
+			$konten .= '<tr>
+						<td>' . $value->nama_barang . '</td>
+						<td>' . $value->deskripsi . '</td>		
+						<td>' . $value->stok . '</td>
+						<td>Read | <a href="#' . $value->id_barang . '" class="linkHapusBarang">Hapus</a> | <a href="#' . $value->id_barang . '" class="linkEditBarang">Edit</a></td>
+					</tr>
+			';
+		}
+
+		$data_json = array(
+			'konten' => $konten
+		);
+
+		echo json_encode($data_json);
 	}
 }
--- application/models/Barang_model.php
+++ application/models/Barang_model.php
@@ -1,28 +1,57 @@
 <?php
-defined('BASEPATH') OR exit('no direct access allowed');
+defined('BASEPATH') or exit('no direct access allowed');
 
-class Barang_model extends CI_Model{
+class Barang_model extends CI_Model
+{
 
 	//Nabiel Izzullah Pansuri -- 19.01.4419
-	public function get_barang(){
+	public function get_barang($cari_nama = '', $cari_desk = '', $cari_stok = '')
+	{
 		$this->db->select('*');
 		$this->db->from('barang');
+		$this->db->where('isDeleted', 0);
+		if ($cari_nama != '' && $cari_nama != null) {
+			$this->db->like('nama_barang', $cari_nama);
+		}
+		if ($cari_desk != '' && $cari_desk != null) {
+			$this->db->like('deskripsi', $cari_nama);
+		}
+		if ($cari_stok != '' && $cari_stok != null) {
+			$this->db->where('stok <=', $cari_stok);
+		}
 		return $this->db->get();
 	}
 
-	public function insert_data($data) {
+	public function insert_data($data)
+	{
 		$this->db->insert('barang', $data);
 	}
 
-	public function get_by_id($id_barang) {
+	public function get_by_id($id_barang)
+	{
 		$this->db->select('*');
 		$this->db->from('barang');
 		$this->db->where('id_barang', $id_barang);
 		return $this->db->get();
 	}
 
-	public function update_data($id_barang, $data) {
+	public function update_data($id_barang, $data)
+	{
 		$this->db->where('id_barang', $id_barang);
 		$this->db->update('barang', $data);
+	}
+
+	public function hapus_data($id)
+	{
+		$this->db->where('id_barang', $id);
+		$this->db->delete('barang');
+	}
+	
+	public function soft_delete_data($id){
+		$data_arr = array(
+			'isDeleted' => 1
+		);
+		$this->db->where('id_barang', $id);
+		$this->db->update('barang', $data_arr );
 	}
 }
